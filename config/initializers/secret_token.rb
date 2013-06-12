5624ca6b2c83c3534d28858c18860897523b4ada
Felix Tsai <felixtsai@yahoo.com>
2013-06-11 17:44:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timestampz2::Application.config.secret_token = 'bba9a41fa9b1109d6b9348194ffa70fb44bd57016163ed872dac0bac731d855a915515677d4728d443112c170b6233ecbe41940bff4e3507cee2c10f29123d8d'
